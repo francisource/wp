9f31057d3dcb345e40417ad1602973bc2823966c
Francis Cruz <francis.link@hotmail.com>
2014-06-04 16:39:07 -0300

    iniciando  estrutura para calculadora

---
+++ include/calculator.php
@@ -0,0 +1,26 @@
+<html>
+	<head>
+		<title></title>
+    <meta charset="utf-8" />
+	</head>
+	<body>
+
+ 
+    <?php include_once 'function.php'; ?>
+
+    <div>
+        <div>
+            <div>
+                <div></div>
+                <div>
+                    
+                </div>
+            </div>
+        </div>
+    </div>
+  
+    </form> 
+
+  </body>
+
+</html>
