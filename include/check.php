3916dfa69afb10dc6e34239ecceb14fa681106db
Francis Cruz <francis.link@hotmail.com>
2014-06-08 09:42:12 -0300

    preparando 2 atividade

--- include/check.php
+++ include/check.php
@@ -4,22 +4,21 @@
     <meta charset="utf-8" />
 	</head>
 	<body>
-
  
-   <?php include_once 'function/function_check.php'; ?>
+  <!-- <?php include_once 'function/function_check.php'; ?> -->
   
-    <form action="" method="POST" role="form"> 
+    <form action="check.php" method="POST" name"check"> 
         
         
         <div class="form-group">
             <label>Nome:</label><input class="form-control" type="text" name="name" />
         </div>
 
-        <div class="form-group">
-            <label>Sexo:</label>
-            <input class="form-control" type="radio" name="sex" value"male"/>Masculino
-            <input class="form-control" type="radio" name="sex" value"female"/>Feminino
-        </div>
+       
+       
+            <input type="radio" name="gender" <?php if (isset($gender) && $gender=="female") echo "checked";?>  value="female">Female
+   <input type="radio" name="gender" <?php if (isset($gender) && $gender=="male") echo "checked";?>  value="male">Male
+
         
         <div class="form-group">
             <label>Idade:</label><input class="form-control" type="number" name="age" /> 
@@ -30,6 +29,30 @@
    
     </form> 
         
+
+   <?php
+
+
+$age = $_POST['age'];
+$name = $_POST['name'];
+
+$gender = "";
+
+ if (empty($_POST["gender"])) {
+     $genderErr = "Gender is required";
+   } else {
+     $gender = test_input($_POST["gender"]);
+   }
+   
+function test_input($data) {
+   $data = trim($data);
+   $data = stripslashes($data);
+   $data = htmlspecialchars($data);
+   return $data;
+}
+
+echo $gender;
+?>
 
   </body>
 
